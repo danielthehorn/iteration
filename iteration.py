2feb869e3911297d35e73cf37151179d1cc56479
Daniel Horn <horndj@s.dcsdk12.org>
2017-10-24 08:25:31 -0600

    Introduce accumulation pattern

--- iteration.py
+++ iteration.py
@@ -3,7 +3,7 @@
 
 # [1, 5, 7 ,8 , 4, 3]
 
-def print(list):
+def print_list(list):
 	# standard for loop with range
 	# for i in range(0, len(list)):
 	# 	print list[i]
@@ -19,13 +19,35 @@ def add_one(list):
 
 	return list
 
+
 def print_scores(names, scores):
 	for i in range(0, len(names)):
 		print names[i] , " scored " , scores[i]
 
 
 # filter pattern
+# exclude a calculation from list members
 def congratulations(names, scores):
 	for i in range(0, len(names)):
 		if (scores[i] == 100):
 			print "Congrats", names[i], "! You got a perfect score!"
+
+
+# accumulation pattern - a type of iteration
+# keep track of other data as we go
+
+def sum(numbers):
+	total = 0
+	for n in numbers:
+		total += n
+
+	return total
+
+def max(numbers):
+	current_max = numbers[0]
+	for n in numbers:
+		if n > current_max:
+			current_max = n
+
+	return current_max
+
--- test.py
+++ test.py
@@ -1,15 +1,21 @@
 from iteration import *
 
-scores = [100, 90, 85, 100, 0]
+#scores = [100, 90, 85, 100, 0]
 
-print(scores)
-scores = add_one(scores)
-print(scores)
+#print_list(scores)
+#scores = add_one(scores)
+#print_list(scores)
 
-names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
+#names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
 
-print(names)
+#print_list(names)
 
-print_scores(names, scores)
+#print_scores(names, scores)
 
-congratulations(names, scores)
+#congratulations(names, scores)
+
+scores = [1, 3, 4, 2, 4, 10]
+
+print sum(scores)
+
+print max(scores)
